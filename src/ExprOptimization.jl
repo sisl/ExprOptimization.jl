04978b47b49975b76f54bf14d41d850222b0806d
Ritchie Lee <ritchie.lee@sv.cmu.edu>
2017-07-24 16:48:32 -0700

    initial, Monte Carlo algorithm working

--- src/ExprOptimization.jl
+++ src/ExprOptimization.jl
@@ -1,5 +1,34 @@
+__precompile__()
+
 module ExprOptimization
 
-# package code goes here
+using ExprRules
+
+export 
+        @ruleset,
+        RuleSet,
+        RuleNode,
+        get_executable,
+
+        loss,
+        optimize,
+        ExprOptResults,
+        MonteCarloParams,
+        GeneticProgramParams
+
+abstract type ExprOptParams end
+
+struct ExprOptResults
+    tree::RuleNode
+    loss::Float64
+    expr::Any
+    alg_results::Any
+end
+
+function loss end #implemented by user
+function optimize end  #implemented by algorithms
+
+include("MonteCarlo/monte_carlo.jl")
+using .MonteCarlo
 
 end # module
---
+++ src/MonteCarlo/monte_carlo.jl
@@ -0,0 +1,39 @@
+
+module MonteCarlo
+
+using ExprRules
+using ..loss
+using ..ExprOptParams
+using ..ExprOptResults
+
+import ..optimize
+
+export MonteCarloParams
+
+struct MonteCarloParams <: ExprOptParams
+    num_samples::Int
+    max_depth::Int
+end
+
+optimize(p::MonteCarloParams, ruleset::RuleSet, typ::Symbol) = monte_carlo(p, ruleset, typ)
+
+"""
+    monte_carlo(p::MonteCarloParams, ruleset::RuleSet, typ::Symbol)
+
+TODO
+"""
+function monte_carlo(p::MonteCarloParams, ruleset::RuleSet, typ::Symbol)
+    best_tree = rand(RuleNode, ruleset, typ, p.max_depth)
+    best_loss = loss(best_tree)
+    for i = 1:p.num_samples
+        tree = rand(RuleNode, ruleset, typ, p.max_depth)
+        los = loss(tree)
+        if los < best_loss
+            best_tree = tree
+            best_loss = los
+        end
+    end
+    ExprOptResults(best_tree, best_loss, get_executable(best_tree, ruleset), nothing)
+end
+
+end #module
