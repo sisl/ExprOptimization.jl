9bcd6f0266888d95b7f7bbc3d4063b9577cc4770
Ritchie Lee <ritchie.lee@gmail.com>
2018-12-10 16:08:56 -0800

    fix typo in PIPEs

--- src/PIPE/PIPEs.jl
+++ src/PIPE/PIPEs.jl
@@ -45,7 +45,7 @@ end
 
 Expression tree optimization using the PIPE algorithm with parameters p, grammar 'grammar', start symbol typ, and loss function 'loss'.  Loss function has the form: los::Float64=loss(node::RuleNode, grammar::Grammar).
 """
-function optimize(p::PIPE, grammar::Grammar, typ::Symbol, loss::Function) 
+function optimize(p::PIPE, grammar::Grammar, typ::Symbol, loss::Function; kwargs...) 
     pipe(p, grammar, typ, loss; kwargs...)
 end
 
